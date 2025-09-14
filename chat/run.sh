5d3d95fbba4c0a732a24200c914aafb80f2317b7
bluecitylights <bluecitylights>
2025-09-14 16:55:30 +0200

    fix

--- chat/run.sh
+++ chat/run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-echo "--- Contents of /app ---"
-ls -la /app/.venv/bin/uvicorn
-ls -la /app/.venv/bin/
-ls -la /app/
-ls -la /app/src/
-echo "------------------------"
-
-
 /app/.venv/bin/python -m uvicorn src.main:app --host 0.0.0.0 --port ${PORT}
--- dashboard/run.sh
+++ dashboard/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/app/.venv/bin/uvicorn src.main:app --host 0.0.0.0 --port ${PORT}
+/app/.venv/bin/python -m uvicorn src.main:app --host 0.0.0.0 --port ${PORT}
