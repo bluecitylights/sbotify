dacbeab3ad14ebab900bf7d086c7f3c168a65d35
bluecitylights <bluecitylights>
2025-09-14 16:49:14 +0200

    fix

--- chat/run.sh
+++ chat/run.sh
@@ -8,4 +8,4 @@ ls -la /app/src/
 echo "------------------------"
 
 
-/app/.venv/bin/uvicorn src.main:app --host 0.0.0.0 --port ${PORT}
+/app/.venv/bin/python -m uvicorn src.main:app --host 0.0.0.0 --port ${PORT}
